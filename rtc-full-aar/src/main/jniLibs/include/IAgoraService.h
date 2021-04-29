38ada7996414991303f6b27392ba09d07e20e9c9
agorabuilder <agorabuilder@agora.io>
2021-04-29 17:49:24 +0800

    3.2.0

--- rtc-full-aar/src/main/jniLibs/include/IAgoraService.h
+++ rtc-full-aar/src/main/jniLibs/include/IAgoraService.h
@@ -8,43 +8,47 @@
 #include "AgoraBase.h"
 
 namespace agora {
-namespace rtc {
-class IRtcEngine;
-}
-namespace rtm {
-class IRtmService;
-}
+    namespace rtc {
+        class IRtcEngine;
+    }
+    namespace rtm {
+        class IRtmService;
+    }
 namespace base {
 
-struct AgoraServiceContext {};
-
-class IAgoraService {
- protected:
-  virtual ~IAgoraService() {}
-
- public:
-  AGORA_CPP_API static void release();
-
-  /** Initializes the engine.
-
-@param context RtcEngine context.
-@return
-- 0: Success.
-- < 0: Failure.
-*/
-  virtual int initialize(const AgoraServiceContext& context) = 0;
-
-  /** Retrieves the SDK version number.
-   * @param build Build number.
-   * @return The current SDK version in the string format. For example, 2.4.0
-   */
-  virtual const char* getVersion(int* build) = 0;
-
-  virtual rtm::IRtmService* createRtmService() = 0;
+struct AgoraServiceContext
+{
 };
 
-}  // namespace base
-}  // namespace agora
+
+class IAgoraService
+{
+protected:
+    virtual ~IAgoraService(){}
+
+public:
+    AGORA_CPP_API static void release ();
+
+	/** Initializes the engine.
+
+    @param context RtcEngine context.
+    @return
+     - 0: Success.
+     - < 0: Failure.
+    */
+    virtual int initialize(const AgoraServiceContext& context) = 0;
+
+    /** Retrieves the SDK version number.
+    * @param build Build number.
+    * @return The current SDK version in the string format. For example, 2.4.0
+    */
+    virtual const char* getVersion(int* build) = 0;
+
+    virtual rtm::IRtmService* createRtmService() = 0;
+};
+
+} //namespace base
+} // namespace agora
 
 /** Gets the SDK version number.
 
@@ -56,16 +60,16 @@ class IAgoraService {
 AGORA_API const char* AGORA_CALL getAgoraSdkVersion(int* build);
 
 /**
- * Creates the RtcEngine object and returns the pointer.
- * @param err Error code
- * @return returns Description of the error code
- */
+* Creates the RtcEngine object and returns the pointer.
+* @param err Error code
+* @return returns Description of the error code
+*/
 AGORA_API const char* AGORA_CALL getAgoraSdkErrorDescription(int err);
 
 /**
- * Creates the Agora Service object and returns the pointer.
- * @return returns Pointer of the Agora Service object
- */
+* Creates the Agora Service object and returns the pointer.
+* @return returns Pointer of the Agora Service object
+*/
 AGORA_API agora::base::IAgoraService* AGORA_CALL createAgoraService();
 
 AGORA_API int AGORA_CALL setAgoraSdkExternalSymbolLoader(void* (*func)(const char* symname));
